7e5dead297c894d63af2c1824ba59fba4c7807a7
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-04-07 23:05:51 +0900

    Add initial code

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+const axios = require("axios");
+const firebase = require("firebase");
+
+const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+const ENDPOINT = config.irkit.endpoint;
+const MESSAGES = config.irkit.messages;
+const ACTIONS = config.actions;
+const path = config.path;
+
+firebase.initializeApp(config.firebase);
+
+const db = firebase.database();
+let initial = true;
+
+db.ref(path).on("value", async function(changedSnapshot) {
+  if (initial) {
+    initial = false;
+    return;
+  }
+
+  const value = changedSnapshot.val();
+  console.log(value);
+  const [command, createdAt, ...options] = value.split(" ");
+  const option = options[0];
+  const commandActions = ACTIONS[command];
+  if (!commandActions) {
+    console.log(`Unsupported command: ${command}`);
+    return;
+  }
+  const action = commandActions[option];
+  if (!action) {
+    console.log(`Unsupported command & options: ${command} / ${options}`);
+    return;
+  }
+
+  const message = MESSAGES[action];
+  if (!message) {
+    console.log(`Failed to find message for command & action: ${command} / ${action}`);
+    return;
+  }
+
+  await axios.post(ENDPOINT, message, {
+    headers: { "X-Requested-With": "axios" }
+  });
+});
+
+process.on("unhandledRejection", console.log);
